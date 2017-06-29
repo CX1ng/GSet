0782c30f354898f0f3b00c4fd25d69be06c2a702
CX1ng <chuanhnag1993@163.com>
2017-06-29 23:31:51 +0800

    格式化代码

--- example/gsetmain.go
+++ example/gsetmain.go
@@ -16,22 +16,22 @@ func main() {
 	}
 
 	set.Add("peach")
-	_,err = set.Add(1)
+	_, err = set.Add(1)
 
 	fmt.Println(err) //1因为不是string，所以插入失败
-	fmt.Println("Size:",set.Size())
+	fmt.Println("Size:", set.Size())
 
 	set.Remove("peach")
-	fmt.Println("Size:",set.Size())
+	fmt.Println("Size:", set.Size())
 
-	fmt.Println("Type:",set.Type())
+	fmt.Println("Type:", set.Type())
 
-	set.MultiAdd("orange","banana",1, "lemon")
-	fmt.Println("Size:",set.Size())
+	set.MultiAdd("orange", "banana", 1, "lemon")
+	fmt.Println("Size:", set.Size())
 
-	set.MultiRemove("watermelon",2, "banana")
-	fmt.Println("Size:",set.Size())
+	set.MultiRemove("watermelon", 2, "banana")
+	fmt.Println("Size:", set.Size())
 
 	set.Clear()
-	fmt.Println("Size:",set.Size())
+	fmt.Println("Size:", set.Size())
 }
