568a4ee4dffcfd756e97e01414312e190fa40c2a
CX1ng <chuanhang1993@163.com>
2017-07-09 23:30:12 +0800

    Test Coverage:24.8%

--- gset_test.go
+++ gset_test.go
@@ -62,3 +62,81 @@ func TestGSet_Add(t *testing.T) {
 		}
 	}
 }
+
+func TestGSet_Remove(t *testing.T) {
+	str := "test"
+	set, err := NewGSet(str)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	testExample := []struct {
+		data interface{}
+		ok   error
+	}{
+		{"test", nil},                     //字符串
+		{123, ErrTypeError},               //int
+		{123.456, ErrTypeError},           //float
+		{true, ErrTypeError},              //bool
+		{make(chan int), ErrTypeError},    //chan
+		{[]int{1, 2, 3}, ErrTypeError},    //slice
+		{make(map[int]int), ErrTypeError}, //map
+		{func() {
+			fmt.Println("Error")
+		}, ErrTypeError},
+		{[3]int{1, 2, 3}, ErrTypeError},
+		{"golang", nil},
+		{"c++", nil},
+	}
+	for _, test := range testExample {
+		_, ok := set.Remove(test.data)
+		if ok != test.ok {
+			t.Errorf("GSet error, when remove value %v\n", test.data)
+		}
+	}
+}
+
+func TestGSet_Clear(t *testing.T) {
+	str := "test"
+	set, err := NewGSet(str)
+	if err != nil {
+		fmt.Println(err)
+	}
+	set.Clear()
+	if set.Size() != 0 {
+		t.Errorf("GSet error, when clear gset\n")
+	}
+}
+
+func TestGSet_Size(t *testing.T) {
+	str := "test"
+	set, err := NewGSet(str)
+	if err != nil {
+		fmt.Println(err)
+	}
+	testExample := []struct {
+		value interface{}
+		cnt   int
+	}{
+		{"test", 1},            //字符串
+		{123, 1},               //int
+		{123.456, 1},           //float
+		{true, 1},              //bool
+		{make(chan int), 1},    //chan
+		{[]int{1, 2, 3}, 1},    //slice
+		{make(map[int]int), 1}, //map
+		{func() {
+			fmt.Println("Error")
+		}, 1},
+		{[3]int{1, 2, 3}, 1},
+		{"golang", 2},
+		{"c++", 3},
+	}
+	for _, test := range testExample {
+		set.Add(test.value)
+		count := set.Size()
+		if count != test.cnt {
+			t.Errorf("GSet Error, When get size of gset")
+		}
+	}
+}
